84037795d57801a06a2ac459e328df16f259fc65
Juan Hebert <jchable@xifralifestyle.com>
2023-01-18 11:28:06 -0600

    countdown component added

---
+++ src/components/Countdown/Countdown.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect, useMemo } from 'react';
+
+const getTimeLeft = targetDate => {
+    const utcTargetDate = new Date(targetDate.toUTCString())
+    const currentDate = new Date();
+    return utcTargetDate - currentDate;
+};
+
+const formatDiffTime = diff => {
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    return { 
+        days: days.toString().padStart(2, '0'), 
+        hours: hours.toString().padStart(2, '0'), 
+        minutes: minutes.toString().padStart(2, '0'), 
+        seconds: seconds.toString().padStart(2, '0')
+    };
+}
+
+
+export default Countdown = ({ targetDate }) => {
+    const [timeLeft, setTimeLeft] = useState(getTimeLeft(targetDate));
+    const formattedTimeleft = useMemo(() => {
+        return formatDiffTime(timeLeft)
+    }, [timeLeft])
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTimeLeft(getTimeLeft(targetDate));
+        }, 500);
+        return () => clearInterval(intervalId);
+    }, [targetDate]);
+
+    return <div>
+        {timeLeft < 0 ? 'The Time Has Come' : <div>
+            <span>{`${formattedTimeleft.days}days ${formattedTimeleft.hours}hrs ${formattedTimeleft.minutes}min ${formattedTimeleft.seconds}sec`}</span>
+        </div>}
+    </div>
+}
--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,3 +1,12 @@
+import styles from './styles.module.sass'
 export default function Dashboard() {
-    
+
+    return <div className={styles.container}>
+        <div className={styles.sidebar}>
+
+        </div>
+        <div className={styles.content}>
+           
+        </div>
+    </div>
 }
