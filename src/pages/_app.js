da66bb9f0431d83276d6a10cb44ed44d22fa3cbf
Christian Sanchez <csanchez@xifralifestyle.com>
2023-01-18 10:59:52 -0600

    Making MainLayout.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
-import '@/styles/globals.css'
+// Layout
+import MainLayout from '@layouts/MainLayout'
+// Styles
+import '@styles/globals.sass'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <MainLayout>
+            <Component {...pageProps} />
+        </MainLayout>
+    )
 }
