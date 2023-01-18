68ea87816f470f896f9bff8c9917bae633c9e535
Christian Sanchez <csanchez@xifralifestyle.com>
2023-01-18 12:08:39 -0600

    StandardJS linter added.

--- src/layouts/InnerContainer/index.jsx
+++ src/layouts/InnerContainer/index.jsx
@@ -1,7 +1,7 @@
 const InnerContainer = ({ children }) => (
     <>
         <div className='w-full h-full flex justify-center'>
-            <div className='w-full max-w-6xl'>
+            <div className='w-full max-w-6xl px-4'>
                 { children }
             </div>
         </div>
