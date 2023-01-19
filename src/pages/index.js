39e7ee8f26ed221285adb5ff0e3720b8a90f67e7
DanteCamacho <dcamacho@decentrauniverse.com>
2023-01-19 13:00:08 -0600

    AnimatedText section added to landing page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,12 +3,14 @@ import Counter from '@components/landing/Counter/Counter'
 import LandingLayout from '@layouts/LandingLayout'
 // Components
 import Hero from '@components/landing/Hero'
+import AnimatedText from '@components/Landing/AnimatedText'
 
 export default function LandingPage() {
     return (
         <>
             <LandingLayout>
                 <Hero />
+                <AnimatedText/>
                 <Counter />
             </LandingLayout>
         </>
