562bc739961da7f51b97144790397ac19a572eb0
jose-jaime-decentra <jrodriguez@xifralifestyle.com>
2023-01-19 11:59:29 -0600

    Better error handling on next connect on error function

--- src/config/apiHandler.js
+++ src/config/apiHandler.js
@@ -1,30 +1,26 @@
 import nextConnect from 'next-connect'
 import dbConnection from '@middlewares/dbConnection'
 
-// BigInt.prototype.toJSON = function() {
-//     return Number(this.toString())
-// }
+// eslint-disable-next-line no-extend-native
+BigInt.prototype.toJSON = function() {
+    return Number(this.toString())
+}
 
 export default function apiHandler() {
     return nextConnect({
         onError: (error, request, response) => {
             console.error(error)
-            if (error.error_code) {
-                response.status(error.error_code).json(
-                    error
-                )
-            } else {
-                response.status(501).json({
-                    code: 501,
-                    message: error.message,
-                    errors: [{ code: error.code ?? 501, mesage: error.message }]
-                })
-            }
+            response.status(error.error_code || 501).json({
+                code: error.error_code || 501,
+                message: error.message,
+                errors: [{ code: error.error_code || 501, mesage: error.message }]
+            })
         },
-
         onNoMatch: (request, response) => {
-            response.status(405)
-            response.json({ message: 'Method not allowed' })
+            response.status(405).json({
+                code: 405,
+                message: 'Method not Allowed'
+            })
         }
     }).use(dbConnection)
 }
