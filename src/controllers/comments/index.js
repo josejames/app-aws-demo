b49b379ae9ef7717f1db259dd51bd2b31e4e0c12
jose-jaime-decentra <jrodriguez@xifralifestyle.com>
2023-01-20 17:24:46 -0600

    create a comment to single post, and read comments on first level

--- src/controllers/comments/index.js
+++ src/controllers/comments/index.js
@@ -3,24 +3,41 @@ import Post from '@models/post'
 import User from '@models/user'
 
 const create = async(userId, postId, body) => {
+    let level = 1
+    let parent = null
+
+    if (body.parentId) {
+        parent = await Comment.findByPk(body.parentId)
+        level = parent.level + 1
+    }
+
     const commentBody = {
         content: body.content,
         postId,
-        userId
+        userId,
+        parentId: parent ? parent.id : null,
+        level
     }
+
     const comment = await Comment.create(commentBody)
     return comment
 }
 
 const retrieve = async (postId, offset, limit) => {
+    // TODO: read and create the levels on the comments
     const comments = await Comment.findAll({
         order: ['id'],
         where: {
             postId
         },
+        level: 1,
+        offset,
+        limit,
         include: [
             {
-                model: User
+                model: User,
+                as: 'user',
+                attributes: ['id', 'name', 'lastName']
             }
         ]
     })
