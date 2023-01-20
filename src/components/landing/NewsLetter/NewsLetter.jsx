a5dc725b7c373193b52c4a9e27c335eb9b2c95cb
Emancillas <emancillas@decentrauniverse.com>
2023-01-20 11:09:41 -0600

    Add animations

--- src/components/landing/NewsLetter/NewsLetter.jsx
+++ src/components/landing/NewsLetter/NewsLetter.jsx
@@ -1,20 +1,41 @@
 import Input from '@components/Input'
+import { motion } from 'framer-motion'
 
 const NewsLetter = () => {
     return (
         <section className="flex flex-col w-full min-h-screen bg-news bg-cover items-center justify-center text-center mt-32">
             <div>
-                <h3 className="text-transparent bg-clip-text text-4xl bg-main-gradient max-w-fit mx-auto">All for One</h3>
-                <h2 className="text-white text-[10rem] leading-none">Come see us in Dubai.</h2>
-                <h3 className="text-white text-4xl max-w-[800px] mx-auto">
+                <motion.h3
+                    transition={{ duration: 1 }}
+                    initial={{ y: -150, opacity: 0, scale: 1.8 }}
+                    whileInView={{ y: 0, opacity: 1, scale: 1 }}
+                    className="text-transparent bg-clip-text text-4xl bg-main-gradient max-w-fit mx-auto"
+                >
+          All for One
+                </motion.h3>
+                <motion.h2
+                    transition={{ duration: 1 }}
+                    initial={{ y: 150, opacity: 0, scale: 1.8 }}
+                    whileInView={{ y: 0, opacity: 1, scale: 1 }}
+                    className="text-white text-[10rem] leading-none">
+          Come see us in Dubai.
+                </motion.h2>
+                <motion.h3
+                    transition={{ duration: 1 }}
+                    initial={{ y: 150, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    className="text-white text-4xl max-w-[800px] mx-auto">
           Get exclusive acces to community updates and an opportunity to join us
           for the event of the year.
-                </h3>
+                </motion.h3>
             </div>
-            <div className="my-16">
+            <motion.div
+                transition={{ duration: 1 }}
+                initial={{ y: 150, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}className="my-16">
                 <p className="my-2 italic">join the waitlist</p>
-                <Input/>
-            </div>
+                <Input />
+            </motion.div>
         </section>
     )
 }
