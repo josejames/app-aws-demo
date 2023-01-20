31ced42bfc191d08ed497c168aa04293d5256e5b
Emancillas <emancillas@decentrauniverse.com>
2023-01-20 11:25:05 -0600

    Fix responsive

--- src/components/landing/NewsLetter/NewsLetter.jsx
+++ src/components/landing/NewsLetter/NewsLetter.jsx
@@ -9,7 +9,7 @@ const NewsLetter = () => {
                     transition={{ duration: 1 }}
                     initial={{ y: -150, opacity: 0, scale: 1.8 }}
                     whileInView={{ y: 0, opacity: 1, scale: 1 }}
-                    className="text-transparent bg-clip-text text-4xl bg-main-gradient max-w-fit mx-auto"
+                    className="text-transparent bg-clip-text text-2xl lg:text-3xl xl:text-4xl bg-main-gradient max-w-fit mx-auto"
                 >
           All for One
                 </motion.h3>
@@ -17,14 +17,14 @@ const NewsLetter = () => {
                     transition={{ duration: 1 }}
                     initial={{ y: 150, opacity: 0, scale: 1.8 }}
                     whileInView={{ y: 0, opacity: 1, scale: 1 }}
-                    className="text-white text-[10rem] leading-none">
+                    className="text-4xl md:text-6xl lg:text-7xl xl:text-8xl 2xl:text-[10rem] leading-none">
           Come see us in Dubai.
                 </motion.h2>
                 <motion.h3
                     transition={{ duration: 1 }}
                     initial={{ y: 150, opacity: 0 }}
                     whileInView={{ y: 0, opacity: 1 }}
-                    className="text-white text-4xl max-w-[800px] mx-auto">
+                    className="max-w-[400px] lg:max-w-[800px] md:text-2xl lg:text-3xl xl:text-4xl mx-16 md:mx-auto">
           Get exclusive acces to community updates and an opportunity to join us
           for the event of the year.
                 </motion.h3>
