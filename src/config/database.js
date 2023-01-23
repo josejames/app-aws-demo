10c6d10d4dd0514c8432f3e973840e2ed0ba3103
jose-jaime-decentra <jrodriguez@xifralifestyle.com>
2023-01-23 14:36:28 -0600

    database dialect module inserted

--- src/config/database.js
+++ src/config/database.js
@@ -1,5 +1,10 @@
 import { Sequelize } from 'sequelize'
+import pg from 'pg'
 
-const sequelize = new Sequelize(process.env.NEXT_PUBLIC_DB_STRING)
+const sequelize = new Sequelize(process.env.NEXT_PUBLIC_DB_STRING, {
+    logging: false,
+    dialectModule: pg,
+    dialect: 'postgres'
+})
 
 export default sequelize
