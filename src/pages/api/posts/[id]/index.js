af0ed73f5e3c1f492785102c915a678533d4b15d
jose-jaime-decentra <jrodriguez@xifralifestyle.com>
2023-01-23 12:28:58 -0600

    update post route and controller method

--- src/pages/api/posts/[id]/index.js
+++ src/pages/api/posts/[id]/index.js
@@ -1,11 +1,16 @@
 import apiHandler from '@config/apiHandler'
-import { retrieveSingle } from '@controllers/post'
+import { retrieveSingle, update } from '@controllers/post'
 
 const handler = apiHandler({
 }).get(async (request, response) => {
     const { id } = request.query
 
     const post = await retrieveSingle(id)
+    return response.send(post)
+}).put(async (request, response) => {
+    const { body } = request
+    const { id } = request.query
+    const post = await update(id, body)
     return response.send(post)
 })
 
