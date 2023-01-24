ae6e0f3be6f209e9c80808c8f0191cf07560dcf6
Dante Camacho <dcamacho@decentrauniverse.com>
2023-01-23 18:51:16 -0600

    Responsiveness improved on login page.

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -7,13 +7,13 @@ import Image from 'next/image'
 export default function Login() {
     return <InnerContainer>
         <div className="w-full h-full flex items-center justify-center xl:pt-40 relative">
-            <div className= "fixed right-0 top-0 flex w-1/4 h-[68vh] items-start">
-                <Image priority fill className='object-cover' alt="" src='/assets/images/login/rightBg.png'/>
+            <div className= "fixed right-0 top-0 flex w-28 h-44 md:w-[30rem] md:h-[46rem] ">
+                <Image priority fill className='object-scale-down' alt="" src='/assets/images/login/rightBg.png'/>
             </div>
-            <div className= "fixed left-0 bottom-0 flex w-1/4 h-[68vh]">
-                <Image priority fill className='object-cover' alt="" src='/assets/images/login/leftBg.png'/>
+            <div className= "fixed left-0 bottom-0 flex  w-28 h-44 md:w-[30rem] md:h-[46rem] ">
+                <Image priority fill className='object-scale-down' alt="" src='/assets/images/login/leftBg.png'/>
             </div>
-            <div className='flex flex-col gap-10 w-full relative items-center'>
+            <div className='flex flex-col gap-10 w-full relative items-center top-32 lg:top-0'>
                 <div className='relative z-5 w-56 md:w-48 h-16'>
                     <Image src='/assets/images/logo-global.png' alt='Mavie global' priority fill className='object-contain' />
                 </div>
