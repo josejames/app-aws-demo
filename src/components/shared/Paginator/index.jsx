28e4bc3747a62b73aad13d3bfed68007b499d42c
Christian Sanchez <csanchez@xifralifestyle.com>
2023-01-24 17:37:10 -0600

    See more section added

--- src/components/shared/Paginator/index.jsx
+++ src/components/shared/Paginator/index.jsx
@@ -1,6 +1,6 @@
 import { IoChevronBack, IoChevronForward } from 'react-icons/io5'
 
-export default function Paginator({ page, onPrev, onNext }) {
+export default function Paginator({ page = 1, onPrev, onNext, disableNext }) {
     const handlePrev = () => {
         onPrev()
     }
@@ -12,11 +12,19 @@ export default function Paginator({ page, onPrev, onNext }) {
     return (
         <div className='bg-main-gradient p-[1px] rounded-lg'>
             <div className='flex items-center gap-[1px] rounded-lg overflow-hidden'>
-                <button className='w-10 h-10 flex justify-center items-center bg-brand-black hover:bg-transparent transition-all ease-in-out' onClick={handlePrev}>
+                <button
+                    disabled={page <= 1}
+                    onClick={handlePrev}
+                    className='w-10 h-10 flex justify-center items-center bg-brand-black hover:bg-transparent disabled:bg-brand-black transition-all ease-in-out'
+                >
                     <IoChevronBack />
                 </button>
-                <div className='w-20 h-10 flex justify-center items-center bg-brand-black'>{ page }</div>
-                <button className='w-10 h-10 flex justify-center items-center bg-brand-black hover:bg-transparent transition-all ease-in-out' onClick={handleNext}>
+                <div className='w-20 h-10 flex justify-center items-center bg-brand-black select-none'>{ page }</div>
+                <button
+                    disabled={disableNext}
+                    onClick={handleNext}
+                    className='w-10 h-10 flex justify-center items-center bg-brand-black hover:bg-transparent disabled:bg-brand-black transition-all ease-in-out'
+                >
                     <IoChevronForward />
                 </button>
             </div>
