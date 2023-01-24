d1230ed5d5e635e212057d1f265cd4cf0378809d
Dante Camacho <dcamacho@decentrauniverse.com>
2023-01-24 09:31:36 -0600

    Login and register forms added to login page.

--- src/components/blog/Login/Button.jsx
+++ src/components/blog/Login/Button.jsx
@@ -1,11 +1,12 @@
 
-export default function Button ({ children }) {
-    return <div
+export default function Button ({ props, children }) {
+    return <button
         href='#'
         className='w-full bg-main-gradient p-[1px] rounded-xl'
+        {...props}
     >
         <div className='w-full flex justify-center items-center cursor-pointer h-full bg-black hover:bg-opacity-0 text-sm md:text-base font-light hover:text-black hover:font-normal py-3 rounded-xl transition-all duration-300 ease-in-out'>
             { children }
         </div>
-    </div>
+    </button>
 }
