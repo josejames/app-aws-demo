ba76b96fa70bcd5f8aa9ccb9c067e8898e88a37a
Christian Sanchez <csanchez@xifralifestyle.com>
2023-01-24 14:22:25 -0600

    Making SeeMore section

--- src/components/blog/SeeMore/index.jsx
+++ src/components/blog/SeeMore/index.jsx
@@ -1,20 +1,25 @@
 // Components
 import PostCard from '@components/shared/PostCard'
+import InnerContainer from '@layouts/InnerContainer'
 
 export default function SeeMore() {
     return (
-        <section className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
-            {
-                Array(10).fill(0).map((item, i) => (
-                    <div key={i} className='w-full max-w-xs'>
-                        <PostCard
-                            title='Lorem Imsun is simple dummy text of the'
-                            content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standa..."
-                            author='Lorem Impsun'
-                        />
-                    </div>
-                ))
-            }
+        <section className='w-full'>
+            <InnerContainer>
+                <div className='w-full grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8 px-4'>
+                    {
+                        Array(10).fill(0).map((item, i) => (
+                            <PostCard
+                                key={i}
+                                title='Lorem Imsun is simple dummy text of the'
+                                content="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standa"
+                                banner='https://public.bnbstatic.com/image/cms/blog/20221209/727053b6-0085-407e-8938-8dfb35e99be7.png'
+                                author='Lorem Impsun'
+                            />
+                        ))
+                    }
+                </div>
+            </InnerContainer>
         </section>
     )
 }
